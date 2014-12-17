209be6486de73d4b845ac2a01d425ddf601ffc0d
tal <talolard@gmail.com>
2014-12-17 23:08:28 +0200

    Fixed broken stuff

--- api/views.py
+++ api/views.py
@@ -1,16 +1,12 @@
-from django.contrib.sessions import serializers
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 import hashlib
 from datetime import datetime
-
+from secret_dispenser.serialisers import  UserSerializer
 @api_view(['POST'])
-class UserSerializer(serializers.Serializer):
 
-    UID= serializers.CharField(max_length=100)
 def create_auth(request):
     '''
     Serialises data from a request which contoains a UID.
@@ -26,3 +22,8 @@ def create_auth(request):
         return Response(serialized.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
+
+def InitateConversation(request):
+    pass
+def RespondToConversation(request):
+    pass
