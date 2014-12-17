5572d8a879beacf42ccc356b7d126a1db72fa7c9
tal <talolard@gmail.com>
2014-12-17 21:43:29 +0200

    Added a user registration view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
+from django.contrib.sessions import serializers
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['POST'])
+class UserSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    username = serializers.CharField(max_length=100)
+def create_auth(request):
+
+    serialized = UserSerializer(data=request.DATA)
+    if serialized.is_valid():
+        User.objects.create_user(
+            serialized.init_data['email'],
+            serialized.init_data['username'],
+            serialized.init_data['password']
+        )
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
