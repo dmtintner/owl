bb59333044be72865c4e77a58ff14cd8358610c6
tal <talolard@gmail.com>
2014-12-17 22:14:09 +0200

    Added conversation model

--- secret_dispenser/models.py
+++ secret_dispenser/models.py
@@ -22,9 +22,26 @@ class Secret(TimeStampModel):
     AuthoringUser = models.Field(models.ForeignKey(User),null=False,db_index=True)
     Content = models.Field(models.TextField())
     NumberOfReplys = models.Field(models.IntegerField,default=1,null=False,index=True)
+    @staticmethod
+    def CreateSecret(AutheringUser,Content):
+        S =Secret()
+        S.AuthoringUser = AutheringUser
+        S.Content =Content
+        S.save()
+        return S
 
 class SecretReply(TimeStampModel):
     Secret = models.ForeignKey(Secret,db_index=True,null=False)
     Replyer =models.ForeignKey(SecretReader)
 
+class Conversation(TimeStampModel):
+    Initiator =models.Field(models.ForeignKey(User),null=False,db_index=True)
+    Replyer = models.Field(models.ForeignKey(User),True,db_index=True)
+    InitialMessage = models.ForeignKey(Secret,db_index=True,null=False)
+    @staticmethod
+    def InitateConversation(Initiator,InitalText):
+        C = Conversation()
+        C.Initiator=Initiator
+        C.S =Secret.CreateSecret(Initiator,InitalText)
+        C.save()
 
